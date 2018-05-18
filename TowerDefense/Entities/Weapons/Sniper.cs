86abc51d15094c4fef738a3a7639646f761127d7
Gijs Alberts <gijs.alberts@windesheim.nl>
2018-05-18 17:01:40 +0200

    removed useless undercase folder

---
+++ TowerDefense/Entities/Weapons/Sniper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Entities.Weapons
+{
+    public class Sniper : Weapon
+    {
+        public Sniper() {
+            attackRange = 10;
+            attackPower = 5;
+            attackInterval = 20;
+        }
+    }
+}
